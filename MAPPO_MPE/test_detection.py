Fixed batch normalization logic in test mode, fixed other minor things.

MAPPO_MPE/test_detection.py
@@ -276,7 +276,7 @@ def get_episode_data(env, runner: Runner_MAPPO_MPE, ref_means, ref_std_devs, do_
         for i in range(runner.args.N):
             result_deques[i].append(results[i])
             taylor_approx_error = np.mean(result_deques[i])
-            if abs(taylor_approx_error - ref_means[i][iter_count]) > 1.5*ref_std_devs[i][iter_count]:
+            if abs(taylor_approx_error - ref_means[i][iter_count]) > 0.6*ref_std_devs[i][iter_count]:
                 if i not in fault_first_detected:
                     print(f" [!!!] Anomaly detected for agent {i} at timestep: {iter_count}. Taylor Appx. Error: {taylor_approx_error}")
                     fault_first_detected[i] = iter_count
@@ -297,27 +297,12 @@ def get_episode_data(env, runner: Runner_MAPPO_MPE, ref_means, ref_std_devs, do_
                         't': iter_count,
                         'contribs': contribs
                     })
-                vulnerable_agent_id = i
             frob_norms_deques[i].append(results_frob_norms[i])
             sec_dir_derivatives_deques[i].append(results_sec_dir_derivatives[i])
 
         metric_vals.append([np.mean(result_deques[i]) for i in range(runner.args.N)])
         frob_norms_list.append([np.mean(frob_norms_deques[i]) for i in range(runner.args.N)])
         sec_dir_derivatives.append([np.mean(sec_dir_derivatives_deques[i]) for i in range(runner.args.N)])
-
-        if vulnerable_agent_id is not None:
-            print(f"\n --- Timestep: {iter_count} ------")
-            frob_norms = frob_norms_list[-1]
-            # Rank frob_norm values from largest to smallest
-            frob_norms_with_indices = [(i, frob_norms[i]) for i in range(len(frob_norms)) if i != vulnerable_agent_id]
-            frob_norms_ranked = sorted(frob_norms_with_indices, key=lambda x: x[1], reverse=True)
-            print(f" --- Frob norm ranking: {frob_norms_ranked}")
-
-            s_dir_derivatives = sec_dir_derivatives[-1]
-            for i in range(runner.args.N):
-                if i == vulnerable_agent_id or s_dir_derivatives[i] >= 0.0:
-                    continue
-                print(f"\t >> Potential Vulnerability for agent {i}. 2nd Dir Derivative: {s_dir_derivatives[i]}")
 
         next_state, reward, done, info = env.step(actions)
         
@@ -360,8 +345,8 @@ def plot_results(results_attacked, attacked_steps, atk_agent_id, ref_means, ref_
         
         # Add green region using ref_means and ref_std_devs
         ref_steps = range(len(ref_means[i]))
-        ref_lower = [ref_means[i][t] - 1.5*ref_std_devs[i][t] for t in range(len(ref_means[i]))]
-        ref_upper = [ref_means[i][t] + 1.5*ref_std_devs[i][t] for t in range(len(ref_means[i]))]
+        ref_lower = [ref_means[i][t] - 0.6*ref_std_devs[i][t] for t in range(len(ref_means[i]))]
+        ref_upper = [ref_means[i][t] + 0.6*ref_std_devs[i][t] for t in range(len(ref_means[i]))]
         ax.fill_between(steps, ref_lower, ref_upper, alpha=0.1, color='green')
         
         ax.plot(steps, attacked_series, 'r-', label='Observed', linewidth=2)

maddpg-pytorch/algorithms/maddpg.py
@@ -15,7 +15,7 @@ class MADDPG(object):
     def __init__(self, agent_init_params, alg_types,
                  gamma=0.95, tau=0.01, lr=0.01, hidden_dim=64,
                  discrete_action=False, obs_shapes=None,
-                 total_action_dim=None):
+                 total_action_dim=None, test_mode=False):
         """
         Inputs:
             agent_init_params (list of dict): List of dicts with parameters to
@@ -38,11 +38,13 @@ class MADDPG(object):
                  hidden_dim=hidden_dim,
                  obs_shapes_critic=obs_shapes,
                  total_action_dim=total_action_dim,
+                 test_mode=test_mode
                  **params)
                for params in agent_init_params]
         else:
             self.agents = [DDPGAgent(lr=lr, discrete_action=discrete_action,
                     hidden_dim=hidden_dim,
+                    test_mode=test_mode,
                     **params)
                 for params in agent_init_params]
         self.agent_init_params = agent_init_params
@@ -294,13 +296,13 @@ class MADDPG(object):
         return instance
 
     @classmethod
-    def init_from_save(cls, filename):
+    def init_from_save(cls, filename, test_mode=False):
         """
         Instantiate instance of this class from file created by 'save' method
         """
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         save_dict = torch.load(filename, map_location=device)
-        instance = cls(**save_dict['init_dict'])
+        instance = cls(test_mode=test_mode, **save_dict['init_dict'])
         instance.init_dict = save_dict['init_dict']
         for a, params in zip(instance.agents, save_dict['agent_params']):
             a.load_params(params)

maddpg-pytorch/test_pettingzoo_detection.py
@@ -328,33 +328,12 @@ def get_episode_data(env, maddpg, config, logdir, ref_means, ref_std_devs, do_at
                         't': cnt,
                         'contribs': contribs
                     })
-                    vulnerable_agent_id = i
             frob_norms_deques[i].append(results_frob_norms[i])
             sec_dir_derivatives_deques[i].append(results_sec_dir_derivatives[i])
 
         metric_vals.append([np.mean(result_deques[i]) for i in range(maddpg.nagents)])
         frob_norms_list.append([np.mean(frob_norms_deques[i]) for i in range(maddpg.nagents)])
         sec_dir_derivatives.append([np.mean(sec_dir_derivatives_deques[i]) for i in range(maddpg.nagents)])
-
-        if vulnerable_agent_id is not None:
-            print(f"\n --- Timestep: {cnt} ------")
-            frob_norms = frob_norms_list[-1]
-            # Rank frob_norm values from largest to smallest
-            frob_norms_with_indices = [(i, frob_norms[i]) for i in range(len(frob_norms)) if i != vulnerable_agent_id]
-            frob_norms_ranked = sorted(frob_norms_with_indices, key=lambda x: x[1], reverse=True)
-            print(f" --- Frob norm ranking: {frob_norms_ranked}")
-
-            s_dir_derivatives = sec_dir_derivatives[-1]
-
-            for i in range(maddpg.nagents):
-                frob_norms_wrt_i = compute_frob_norms(maddpg, obs, list(actions.values()), env.action_space, i)
-                print(f" ~ [Frob Norms] w.r.t. agent {i}: {frob_norms_wrt_i}")
-                sod_wrt_i = compute_2nd_ord_dir_derivatives(maddpg, obs, list(actions.values()), env.action_space, i)
-                print(f" ~ [2nd Order Derivatives] w.r.t. agent {i}: {sod_wrt_i}\n")
-
-                if i == vulnerable_agent_id or s_dir_derivatives[i] >= 0.0:
-                    continue
-                print(f" >> Potential Vulnerable Position for agent {i} wrt faulty agent. 2nd Dir Derivative: {s_dir_derivatives[i]}")
 
         next_obs, rewards, dones, infos = env.step(actions)
         episode_reward += np.sum([rewards[:,i] if env.agent_types[i] != 'adversary' else np.zeros_like(rewards[:,i]) for i in range(maddpg.nagents)])
@@ -401,15 +380,18 @@ def plot_results(results_attacked, attacked_steps, atk_agent_id, ref_means, ref_
         ref_upper = [ref_means[i][t] + ref_std_devs[i][t] for t in range(len(ref_means[i]))]
         ax.fill_between(steps, ref_lower, ref_upper, alpha=0.1, color='green')
         
-        ax.plot(steps, attacked_series, 'r-', label='Under Attack', linewidth=2)
-        ax.plot(steps, ref_means[i], 'g--', label='Normal (Mean)', linewidth=2)
+        ax.plot(steps, attacked_series, 'r-', label='Observed', linewidth=2)
+        ax.plot(steps, ref_means[i], 'g--', label='Reference (Mean)', linewidth=2)
         
         # Mark attacked timesteps with vertical lines
         if i == atk_agent_id and attacked_steps:
-            for attack_step in attacked_steps:
-                ax.axvline(x=attack_step, color='orange', linestyle=':', alpha=0.5, linewidth=1.5)
-            # Add legend entry for attack markers
-            ax.axvline(x=attacked_steps[0], color='orange', linestyle=':', alpha=0.5, linewidth=1.5, label='Attacked Steps')
+            # for attack_step in attacked_steps:
+            #     ax.axvline(x=attack_step, color='orange', linestyle=':', alpha=0.5, linewidth=1.5)
+            # # Add legend entry for attack markers
+            # ax.axvline(x=attacked_steps[0], color='orange', linestyle=':', alpha=0.5, linewidth=1.5, label='Attacked Steps')
+            start = min(attacked_steps)
+            end = max(attacked_steps)
+            ax.axvspan(start, end, color='red', alpha=0.1, label='Attacked Region')
         
         ax.set_xlabel('Step')
         ax.set_ylabel('Taylor Delta Error')
@@ -749,7 +731,7 @@ def save_matrix_to_files(matrix, attacked_steps, attacked_agent_id, total_agents
 
 
 def run(config):
-    maddpg = MADDPG.init_from_save(config.model_path)
+    maddpg = MADDPG.init_from_save(config.model_path, test_mode=True)
 
     # create a log directory under runs/<env_id>/<timestamp> using os and getcwd
     cwd = os.getcwd()
@@ -797,7 +779,7 @@ def run(config):
                 # ref_std_devs[agent_id].append(float(row[8]))
 
     attacked_agent_id = config.attack_agent_id  # specify the agent to attack
-    seed = 42
+    seed = 23
 
     results_normal, _, frob_norms_normal, sec_dir_derivatives_normal, _, _ = get_episode_data(env, maddpg, config, logdir, ref_means, ref_std_devs, do_attack=False, atk_agent_id=attacked_agent_id, seed=seed)
 

maddpg-pytorch/utils/networks.py
@@ -8,7 +8,7 @@ class MLPNetwork(nn.Module):
     MLP network (can be used as value or policy)
     """
     def __init__(self, input_dim, out_dim, hidden_dim=64, nonlin=F.relu,
-                 constrain_out=False, norm_in=True, discrete_action=True):
+                 constrain_out=False, norm_in=True, discrete_action=True, test_mode=False):
         """
         Inputs:
             input_dim (int): Number of dimensions in input
@@ -34,6 +34,8 @@ class MLPNetwork(nn.Module):
             self.out_fn = F.tanh
         else:  # logits for discrete action (will softmax later)
             self.out_fn = lambda x: x
+        
+        self.test_mode = test_mode
 
     def forward(self, X):
         """
@@ -45,8 +47,8 @@ class MLPNetwork(nn.Module):
         # h1 = self.nonlin(self.fc1(self.in_fn(X)))
         # h1 = self.nonlin(self.fc1(X))
         # h2 = self.nonlin(self.fc2(h1))
-        # h1 = F.tanh(self.fc1(self.in_fn(X)))
-        h1 = F.tanh(self.fc1(X))
+        
+        h1 = F.tanh(self.fc1(self.in_fn(X))) if not self.test_mode else F.tanh(self.fc1(X))
         h2 = F.tanh(self.fc2(h1))
         out = self.out_fn(self.fc3(h2))
         return out
@@ -57,7 +59,7 @@ class CNNNetwork(nn.Module):
     Processes image observations through convolutional layers then MLP
     """
     def __init__(self, input_shape, out_dim, hidden_dim=512, nonlin=F.relu,
-                 constrain_out=False, norm_in=True, discrete_action=True):
+                 constrain_out=False, norm_in=True, discrete_action=True, test_mode=False):
         """
         Inputs:
             input_shape (tuple): Shape of input images (height, width, channels) e.g., (210, 160, 3)
@@ -117,6 +119,8 @@ class CNNNetwork(nn.Module):
             self.out_fn = F.tanh
         else:  # logits for discrete action (will softmax later)
             self.out_fn = lambda x: x
+        
+        self.test_mode = test_mode
     
     def preprocess_image(self, x):
         """
@@ -155,7 +159,7 @@ class CNNNetwork(nn.Module):
         flattened = conv_out.reshape(conv_out.size(0), -1)
         
         # Pass through MLP layers with batch normalization
-        h1 = self.nonlin(self.fc1(self.in_fn(flattened)))
+        h1 = self.nonlin(self.fc1(self.in_fn(flattened))) if not self.test_mode else self.nonlin(self.fc1(flattened))
         h2 = self.nonlin(self.fc2(h1))
         out = self.out_fn(self.fc3(h2))
         